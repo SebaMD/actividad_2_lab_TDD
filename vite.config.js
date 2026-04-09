53d56d1fad06c9f32965b5547af02b115944cd7e
Sebastian <sebamedina273@gmail.com>
2026-04-09 17:13:01 -0400

    mod: cambie el nombre del sitio

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/actividad_2_lab_TDD/'
+  base: '/landing-page-tesis/'
 })
